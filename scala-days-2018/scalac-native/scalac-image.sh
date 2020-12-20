120f7b1737aaf202692cc8fc6deaf69be05d04fd
Darrell Schiebel <darrell@schiebel.us>
2020-12-19 22:35:58 -0500

    changes need to build native scalac circa 202{0,1}

--- scala-days-2018/scalac-native/scalac-image.sh
+++ scala-days-2018/scalac-native/scalac-image.sh
@@ -12,4 +12,7 @@ for filename in $SCALA_LIB/*.jar; do
     SCALA_LIB_CLASSPATH=$filename:$SCALA_LIB_CLASSPATH
 done
 
-$GRAALVM_HOME/bin/native-image --no-fallback --initialize-at-build-time -cp $SCALA_LIB_CLASSPATH:$PWD/scalac-substitutions/target/scala-2.12/scalac-substitutions_2.12-0.1.0-SNAPSHOT.jar scala.tools.nsc.Main -H:SubstitutionResources=substitutions.json,substitutions-2.12.json -H:ReflectionConfigurationFiles=scalac-substitutions/reflection-config.json -H:Name=scalac $@
+$GRAALVM_HOME/bin/native-image --no-fallback --initialize-at-build-time \
+                               --initialize-at-run-time=scala.tools.nsc.profile.RealProfiler$,scala.tools.nsc.profile.ExtendedThreadMxBean,scala.tools.nsc.classpath.FileBasedCache$,sun.awt.dnd,sun.java2d.SurfaceData \
+                               --report-unsupported-elements-at-runtime \
+                               -cp $SCALA_LIB_CLASSPATH:$PWD/scalac-substitutions/target/scala-2.12/scalac-substitutions_2.12-0.1.0-SNAPSHOT.jar scala.tools.nsc.Main -H:SubstitutionResources=substitutions.json,substitutions-2.12.json -H:ReflectionConfigurationFiles=scalac-substitutions/reflection-config.json -H:Name=scalac $@
